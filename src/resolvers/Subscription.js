d5e44ac3cbac92fc9265b769d85fd08bfcf03d75
lakith <lakith1995@gmail.com>
2020-06-24 14:37:05 +0530

    add post create subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -13,6 +13,18 @@ const Subscription = {
       return pubsub.asyncIterator("count");
     },
   },
+
+  comment: {
+    subscribe(parent, {postId}, { pubsub, db }, info) { 
+        const post = db.posts.find(post => post.id === postId && post.published)
+
+        if(!post) {
+            throw new Error("Post Not Found")
+        }
+
+        return pubsub.asyncIterator(`comment ${postId}`)
+    }
+  }
 };
 
 export default Subscription;
