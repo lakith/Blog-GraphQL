cea8b3471331303c2d45b7fa568070bccd168ef7
lakith <lakith1995@gmail.com>
2020-06-24 20:16:18 +0530

    comment subscription endpoits added

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,19 +1,4 @@
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubsub }, info) {
-      let count = 0;
-
-      setInterval(() => {
-        count++;
-        pubsub.publish("count", {
-          count,
-        });
-      }, 1000);
-
-      return pubsub.asyncIterator("count");
-    },
-  },
-
   comment: {
     subscribe(parent, {postId}, { pubsub, db }, info) { 
         const post = db.posts.find(post => post.id === postId && post.published)
